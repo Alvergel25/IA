cb46cc82aa60832602154c27176a7e506a5d5e2f
Alvergel25 <alvergel2005@gmail.com>
2024-11-04 10:14:49 +0100

    atalcaja

---
+++ Assets/Resources/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float walkingSpeed, runningSpeed, acceleration, jumpforce, mouseSens, sphereRadius;
+    public string groundName;
+
+    private Rigidbody rb;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
